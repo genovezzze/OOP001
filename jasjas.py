71e7acf78e1dabb0bd0546d7e45ddbc33b5f3c0a
Artemijs Mihails Lučins <artemijs.lucins@ventspils.edu.lv>
2025-01-16 08:32:37 +0200

    1

--- jasjas.py
+++ jasjas.py
@@ -1,1 +1,9 @@
+class Dzivnieks:
+    def __init__(self, vards, kajas):
+        self.name=vards
+        self.kajas=kajas
+    def skanja(self):
+        print("random animal noise")
 
+f1=Dzivnieks("Gauja", 4)
+d1.skanja()
