0d3a4ccaac758a6be274c1d88e16944b3e2ca861
Artemijs Mihails Lučins <artemijs.lucins@ventspils.edu.lv>
2025-01-16 09:25:21 +0200

    3

--- jasjas.py
+++ jasjas.py
@@ -1,21 +1,52 @@
-class Dzivnieks:
-    def __init__(self, vards, kajas):
-        self.vards=vards
-        self.kajas=kajas
+class Dzivnieki:
+    def __init__(self, vards, kajas): 
+        self.vards = vards
+        self.kajas = kajas
+    
     def skanja(self):
         print("random animal noise")
-    def __str__(self):
+    
+    def __str__(self): 
         return f"{self.vards} un {self.kajas} kajas"
 
-class Suns(Dzivnieks):
-    def __init__(self, vards, kajas):
+class Suns(Dzivnieki):
+    def __init__(self, vards, kajas): 
         super().__init__(vards, kajas)
-        self.vards="Komisars "+self.vards
+        self.vards = "Komisars " + self.vards
+    
+    def skanja(self):
+        print("Vau vau")
 
-d1=Dzivnieks("Gauja", 4)
+class Kakjis(Dzivnieki):
+    def __init__(self, vards, kajas): 
+        super().__init__(vards, kajas)
+        self.vards = "Minkans " + self.vards
+    
+    def skanja(self):
+        print("MurMjau")
+
+d1 = Dzivnieki("Gauja", 4)
 print(d1)
 d1.skanja()
 
-s1=Suns("Reksis", 4)
+s1 = Suns("Reksis", 4)
 print(s1)
 s1.skanja()
+
+s2 = Kakjis("Muris", 4)
+print(s2)
+s2.skanja()
+
+dzivniekuSaraksts = []
+dzivniekuSaraksts.append(Suns("Reksis", 3))
+dzivniekuSaraksts.append(Suns("Volvis", 4))
+dzivniekuSaraksts.append(Suns("Caps", 4))
+dzivniekuSaraksts.append(Kakjis("Murcis", 4))
+dzivniekuSaraksts.append(Kakjis("Burkaans", 4))
+dzivniekuSaraksts.append(Dzivnieki("Gauja", 4))
+
+print("#####################")
+
+for dzivnieks in dzivniekuSaraksts:
+    print(dzivnieks)
+    dzivnieks.skanja()
